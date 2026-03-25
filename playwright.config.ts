81d18b6af595d14d95d97e5c5e82a9bc9cd23b28
Maciel98 <135555222+Maciel98@users.noreply.github.com>
2026-03-25 15:34:46 -0400

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,13 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+
+    /* Capture screenshot on failure */
+    screenshot: 'only-on-failure',
+
+    /* Capture video on failure */
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
